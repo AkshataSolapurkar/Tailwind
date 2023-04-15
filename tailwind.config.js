f64aeba820d4f8fbf3020fd9f0f429cf8e9edd2c
AkshataSolapurkar <solapurkarakshata@gmail.com>
2023-04-15 17:21:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./disk/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        pacifico : ["'Pacifico'", 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
